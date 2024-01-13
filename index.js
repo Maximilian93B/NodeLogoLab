9d1ffcad2bb0f626ad0d7f3119868a3dc2f17129
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-01-12 21:37:35 -0400

    added boiler for questions array

---
+++ index.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+
+
+
+// Prompt using inquirer to display Array of input questions 
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters for your logo:',
+        validate: input => input.length <= 3 && input.length > 0
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color for the text', // will have to figure out a way for user to select color and validate input 
+        //add validation for color choice 
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape for the logo:',
+        choices: ['Circle', 'Triangle', 'Square']   
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color for the shape'
+        //add color validation same as foor 'textColor'
+    },
+]).then(answers => {
+    //Use input to generate SVG
+})
